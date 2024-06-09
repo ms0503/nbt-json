3b8a97af0d02ff41cd6783772c91537cf1bc4d4c
Sora Tonami <ms0503@outlook.com>
2024-06-09 18:17:32 +0900

    add i18n

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use nbt_json::nbt_to_json;
 use nbt_json::TagType;
+use rust_i18n::i18n;
+use rust_i18n::t;
 use std::collections::HashMap;
 use std::env;
 use std::path::Path;
 use std::process;
 
+i18n!("locales", fallback = ["en_US", "ja_JP"]);
+
 fn main() {
+    if let Ok(lang) = env::var("LANG") {
+        if let Some(lang) = lang.split(".").nth(0) {
+            rust_i18n::set_locale(&lang);
+        }
+    }
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("must be specified filename");
+        eprintln!("{}", t!("error.no-filename"));
         process::exit(1);
     }
     if !Path::new(&args[1]).exists() {
-        eprintln!("specified file '{}' is not found", args[1]);
+        eprintln!("{}", t!("error.file-not-found", filename = args[1]));
         process::exit(1);
     }
     let mut json = HashMap::<String, TagType>::new();
